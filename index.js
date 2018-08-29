20899f83e402ccf3cbf39d6b7ac0a1dc98771459
sondos alansi <sondos.alansi@yahoo.com>
2018-08-29 19:15:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName='bob';
 var bestCustomer;
-const 
+const leastFavoriteCustomer;
 
 function upperCaseCustomerName(){
  customerName=customerName.toUpperCase();
